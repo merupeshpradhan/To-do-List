f40f8a47e2e0355902019dcbea476294110c3ec8
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-03-17 11:19:23 +0530

    Todo List Overflow adding and git in center

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,31 @@ import ListToDo from "./TodoAllList/ListToDo";
 function App() {
   const [ToDoItem, setToDoItem] = useState([]);
 
+  // Add List Item
   let addList = (inputText) => {
-    if (inputText !== "")
-     setToDoItem([...ToDoItem, inputText]);
+    if (inputText !== "") setToDoItem([...ToDoItem, inputText]);
   };
 
+  // Delete List Item
   const deleteListItem = (key) => {
     let newListTodo = [...ToDoItem];
     newListTodo.splice(key, 1);
     setToDoItem([...newListTodo]);
   };
+
   return (
     <div className="bg-fuchsia-100 h-[100vh] flex items-center justify-center">
       <div className="w-[90vh] h-[70vh] bg-fuchsia-300 rounded-2xl">
         <InputToDo addList={addList} />
 
         <div className="flex justify-center mt-8">
-          <div className="h-[55vh] w-[40vw] border-2 border-fuchsia-50 rounded-2xl p-3">
-            <h1 className=" text-xl font-bold text-indigo-600 border-b-2 w-[6vw]">
+          <div className="h-[50vh] w-1/2 border-2 border-fuchsia-50 rounded-2xl p-3">
+            <h1 className=" text-xl font-bold text-indigo-600 border-b-2 w-1/2">
               TODO List's
             </h1>
-            {ToDoItem.map((listItem, i) => {
-              return (
-                <ListToDo
-                  key={i}
-                  index={i}
-                  item={listItem}
-                  deleteItem={deleteListItem}
-                />
-              );
-            })}
+            <div className=" flex justify-center overflow-y-auto overflow-bg-red-300  h-[90%]">
+              <ListToDo items={ToDoItem} deleteItem={deleteListItem} />
+            </div>
           </div>
         </div>
       </div>
--- src/TodoAllList/ListToDo.jsx
+++ src/TodoAllList/ListToDo.jsx
@@ -3,15 +3,20 @@ import React from "react";
 function ListToDo(props) {
   return (
     <ul className="pt-5">
-      <li className="border-2 border-blue-300 bg-white w-[15vw] h-[4vh] rounded-lg text-center flex justify-between items-center">
-        <span className="pl-2">{props.item}</span>
-        <span className="pr-3">
-          <i
-            className="fa-solid fa-trash-can text-black hover:text-red-600 hover:scale-125"
-            onClick={(e) => props.deleteItem(props.index)}
-          ></i>
-        </span>
-      </li>
+      {props.items.map((lisItem, i) => (
+        <li
+          key={i}
+          className="border-2 border-blue-300 bg-white w-[15vw] h-[4vh] mb-2 rounded-lg text-center flex justify-between items-center"
+        >
+          <span className="pl-2">{lisItem}</span>
+          <span className="pr-3">
+            <i
+              className="fa-solid fa-trash-can text-black hover:text-red-600 hover:scale-125"
+              onClick={(e) => props.deleteItem(i)}
+            ></i>
+          </span>
+        </li>
+      ))}
     </ul>
   );
 }
